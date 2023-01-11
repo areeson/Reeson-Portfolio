431af3c7243a81ffe14012b60abad8866fa325ca
Andrew <62225671+areeson@users.noreply.github.com>
2023-01-11 16:21:54 -0600

    Added close window function plus UX example tweaks

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,20 +7,21 @@ import { cartData } from '../data/dummy';
 import { Button } from '.';
 
 const Cart = () => {
-  const { currentColor } = useStateContext();
+  const { currentColor, setIsClicked } = useStateContext();
 
   return (
     <div className="bg-half-transparent w-full fixed nav-item top-0 right-0 ">
       <div className="float-right h-screen  duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8">
         <div className="flex justify-between items-center">
-          <p className="font-semibold text-lg">Shopping Cart</p>
-          <Button
-            icon={<MdOutlineCancel />}
-            color="rgb(153, 171, 180)"
-            bgHoverColor="light-gray"
-            size="2xl"
-            borderRadius="50%"
-          />
+          <p className="font-semibold text-lg">Shopping Cart (UX Example)</p>
+          <button 
+            type='button'
+            onClick={() => setIsClicked(false)}
+            style={{ color: 'rgb(153, 171, 180)', borderRadius:'50%' }}
+            className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray'
+          >
+            <MdOutlineCancel />
+          </button> 
         </div>
         {cartData?.map((item, index) => (
           <div key={index}>
@@ -46,11 +47,11 @@ const Cart = () => {
         <div className="mt-3 mb-3">
           <div className="flex justify-between items-center">
             <p className="text-gray-500 dark:text-gray-200">Sub Total</p>
-            <p className="font-semibold">$890</p>
+            <p className="font-semibold">$265</p>
           </div>
           <div className="flex justify-between items-center mt-3">
             <p className="text-gray-500 dark:text-gray-200">Total</p>
-            <p className="font-semibold">$890</p>
+            <p className="font-semibold">$283.55</p>
           </div>
         </div>
         <div className="mt-5">
